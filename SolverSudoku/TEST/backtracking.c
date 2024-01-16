3664ec54b26d1d11ac3b6fef52015f9f295a4150
brieuc gouret <brieuc.gouret@etudiant.univ-rennes1.fr>
2024-01-16 10:01:05 +0100

    le programme marche

--- SolverSudoku/TEST/backtracking.c
+++ SolverSudoku/TEST/backtracking.c
@@ -2,37 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <time.h>
 
 #define N 4
 #define TAILLE (N*N)
 
-
 typedef int tGrille[TAILLE][TAILLE];
 
-
-void chargerGrille(tGrille grille,char *nomFichier);
-
+void chargerGrille(tGrille grille, char *nomFichier);
 void afficherGrille(tGrille grille);
-
 bool backtracking(tGrille grille, int numeroCase);
-    
 bool absentSurLigne(int valeur, tGrille grille, int ligne);
-
 bool absentSurColonne(int valeur, tGrille grille, int colonne);
-
 bool absentSurBloc(int valeur, tGrille grille, int ligne, int colonne);
 
-int main(){
+int main() {
+    clock_t begin = clock();
     tGrille grille;
     char nomgrille[30] = "";
-    
-    
+
     chargerGrille(grille, nomgrille);
-    
+
     printf("Grille initiale :\n");
     afficherGrille(grille);
 
-    
     if (backtracking(grille, 0)) {
         printf("\nGrille résolue :\n");
         afficherGrille(grille);
@@ -40,81 +33,86 @@ int main(){
         printf("\nLa grille ne peut pas être résolue.\n");
     }
 
+    clock_t end = clock();
+    double tmpsCPU = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Temps CPU = %.3f secondes\n", tmpsCPU);
+
     return 0;
 }
 
-
-void backtracking(tGrille grille, int numeroCase){
-    int ligne;
-    int colonne;
+bool backtracking(tGrille grille, int numeroCase) {
+    int ligne, colonne;
     bool resultat = false;
-    if (numeroCase = TAILLE * TAILLE){
-        //toutes les cases sont traitées
+
+    if (numeroCase == TAILLE * TAILLE) {
+        // toutes les cases sont traitées
         resultat = true;
-    }
-    else {
+    } else {
         ligne = numeroCase / TAILLE;
         colonne = numeroCase % TAILLE;
-        if (grille[ligne][colonne]!=0){
-            //si la case n'est pas vide, on passe à la suivante
-            resultat = backtracking(grille, numeroCase+1);
-        }
-        else {
-            for (int valeur=1; valeur < TAILLE; valeur++){
-                if (absentSurLigne(int valeur,tGrille grille, int ligne) && absentSurColonne(int valeur, tGrille grille, int colonne) && absentSurBloc(int valeur, tGrille grille, int ligne, int colonne)){
+        if (grille[ligne][colonne] != 0) {
+            // si la case n'est pas vide, on passe à la suivante
+            resultat = backtracking(grille, numeroCase + 1);
+        } else {
+            for (int valeur = 1; valeur <= TAILLE; valeur++) {
+                if (absentSurLigne(valeur, grille, ligne) && absentSurColonne(valeur, grille, colonne) &&
+                    absentSurBloc(valeur, grille, ligne, colonne)) {
                     grille[ligne][colonne] = valeur;
-                }
-                if (backtracking(tGrille, numeroCase+1) = true) {
-                    resultat = true;
-                }
-                else {
-                    grille[ligne][colonne] = 0;
+
+                    if (backtracking(grille, numeroCase + 1)) {
+                        resultat = true;
+                        break; 
+                    } else {
+                        grille[ligne][colonne] = 0;  
+                    }
                 }
             }
         }
-
     }
     return resultat;
 }
 
-bool absentSurLigne(int valeur, tGrille grille, int ligne){
+bool absentSurLigne(int valeur, tGrille grille, int ligne) {
     int j;
     bool possible = true;
     for (j = 0; j < TAILLE; j++) {
-        if (grille[ligne][j] == valeur && j != numColonne) {
+        if (grille[ligne][j] == valeur) {
             possible = false;
+            break;  // Pas besoin de vérifier le reste de la ligne
         }
     }
     return possible;
 }
 
-bool absentSurColonne(int valeur, tGrille grille, int colonne){
+bool absentSurColonne(int valeur, tGrille grille, int colonne) {
     int i;
     bool possible = true;
     for (i = 0; i < TAILLE; i++) {
-        if (grille[i][colonne].valeur == valeur && i != numLignes) {
+        if (grille[i][colonne] == valeur) {
             possible = false;
+            break;  // Pas besoin de vérifier le reste de la colonne
         }
     }
     return possible;
 }
 
-bool absentSurBloc(int valeur, tGrille grille, int ligne, int colonne){
+bool absentSurBloc(int valeur, tGrille grille, int ligne, int colonne) {
     int coinBlocColonne, coinBlocLigne;
     bool possible = true;
-    coinBlocLigne = (numLignes / 3) * 3;
-    coinBlocColonne = (numColonne / 3) * 3;
-    for (int i = coinBlocLigne; i < coinBlocLigne + 3; i++) {
-        for (int j = coinBlocColonne; j < coinBlocColonne + 3; j++) {
-            if (grilleCase[i][j].valeur == valeur && (i != numLignes || j != numColonne)) {
+    coinBlocLigne = (ligne / N) * N;
+    coinBlocColonne = (colonne / N) * N;
+    for (int i = coinBlocLigne; i < coinBlocLigne + N; i++) {
+        for (int j = coinBlocColonne; j < coinBlocColonne + N; j++) {
+            if (grille[i][j] == valeur) {
                 possible = false;
+                break;  // Pas besoin de vérifier le reste du bloc
             }
         }
     }
     return possible;
 }
 
-void chargerGrille(tGrille g,char *nomFichier) {
+void chargerGrille(tGrille g, char *nomFichier) {
     FILE *f;
     printf("Nom du fichier ?\n");
     scanf("%s", nomFichier);
